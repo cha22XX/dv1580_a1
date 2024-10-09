632872182502fad6f2ef526c6eabd8810ba93506
cha22XX <129762303+cha22XX@users.noreply.github.com>
2024-10-09 18:18:29 +0200

    Update memory_manager.h

--- memory_manager.h
+++ memory_manager.h
@@ -26,4 +26,33 @@ void mem_deinit(void);
 
 #endif // MEMORY_MANAGER_H*/
 
+#ifndef MEMORY_MANAGER_H  
+#define MEMORY_MANAGER_H  
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stddef.h>  
+
+
+#define POOL_SIZE 81920000 
+#define MAX_BLOCKS 100000      
+
+typedef struct Block {
+    size_t size;
+    int free;     
+} Block;
+
+static char memory_pool[POOL_SIZE];  
+static Block block_list[MAX_BLOCKS]; 
+static size_t num_blocks = 0; 
+
+void mem_init(size_t size);
+void* mem_alloc(size_t size);
+void mem_free(void* block);
+void* mem_resize(void* block, size_t size);
+void mem_deinit(void);
+
+#endif
+
 
