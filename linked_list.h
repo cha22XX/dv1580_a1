d9ca7e90121c6082736985072b979a79dd01d8b2
cha22XX <129762303+cha22XX@users.noreply.github.com>
2024-10-17 18:02:20 +0200

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -1,4 +1,4 @@
-#ifndef LINKED_LIST_H
+/*#ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
 #include <stdint.h>
@@ -20,4 +20,36 @@ void list_display_range(Node** head, Node* start_node, Node* end_node);
 int list_count_nodes(Node** head);                        
 void list_cleanup(Node** head);                          
 
+#endif*/
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <pthread.h>
+
+// تعريف عقدة القائمة المرتبطة
+typedef struct Node {
+    int data;
+    struct Node* next;
+    pthread_mutex_t node_mutex; // قفل لكل عقدة
+} Node;
+
+void list_init(Node** head, size_t size);               
+void list_insert(Node** head, uint16_t data);                  
+void list_insert_after(Node* prev_node, uint16_t data);        
+void list_insert_before(Node** head, Node* next_node, uint16_t data);  
+void list_delete(Node** head, uint16_t data);                  
+Node* list_search(Node** head, uint16_t data);                
+void list_display(Node** head);                         
+void list_display_range(Node** head, Node* start_node, Node* end_node); 
+int list_count_nodes(Node** head);                        
+void list_cleanup(Node** head);                          
+
 #endif
+
