be4a425aec6b82a4e37bb16f6eb0af6dd21aa943
cha22XX <129762303+cha22XX@users.noreply.github.com>
2024-10-17 18:01:42 +0200

    Update memory_manager.h

--- memory_manager.h
+++ memory_manager.h
@@ -25,7 +25,7 @@ void* mem_resize(void* block, size_t new_size);
 void mem_deinit(void);
 
 #endif // MEMORY_MANAGER_H*/
-
+/*
 #ifndef MEMORY_MANAGER_H  
 #define MEMORY_MANAGER_H  
 
@@ -53,6 +53,39 @@ void mem_free(void* block);
 void* mem_resize(void* block, size_t size);
 void mem_deinit(void);
 
-#endif
+#endif*/
+
+#ifndef MEMORY_MANAGER_H  
+#define MEMORY_MANAGER_H  
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+#define POOL_SIZE 81920000 
+#define MAX_BLOCKS 100000      
+
+
+typedef struct Block {
+    void* address; // عنوان الكتلة
+    size_t size;   // حجم الكتلة
+    bool is_free;  // إذا كانت الكتلة فارغة
+    struct Block* next; // الكتلة التالية
+} Block;
+
+
+
+
+void mem_init(size_t size);
+void* mem_alloc(size_t size);
+void mem_free(void* block);
+void* mem_resize(void* block, size_t size);
+void mem_deinit(void);
+
+#endif 
 
 
