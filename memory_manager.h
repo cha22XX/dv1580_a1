3803eda33c247310084d21b16c910d10be315ca0
cha22XX <129762303+cha22XX@users.noreply.github.com>
2024-10-20 12:24:56 +0200

    Add files via upload

---
+++ memory_manager.h
@@ -0,0 +1,35 @@
+#ifndef MEMORY_MANAGER_H  
+#define MEMORY_MANAGER_H  
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stddef.h>  
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+#define POOL_SIZE 81920000 
+#define MAX_BLOCKS 100000      
+
+
+typedef struct Block {
+    void* address; // 
+    size_t size;   // 
+    bool is_free;  // 
+    struct Block* next; // 
+} Block;
+
+
+void* memory_pool; // 
+Block* head_pool;  // 
+
+void mem_init(size_t size);
+void* mem_alloc(size_t size);
+void mem_free(void* block);
+void* mem_resize(void* block, size_t size);
+void mem_deinit(void);
+
+#endif 
+
+
